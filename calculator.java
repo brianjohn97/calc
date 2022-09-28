4d6612615c3c21e2e0960edca3204c33159e13f8
brianjohn97 <johnson.brian997@gmail.com>
2022-09-28 13:30:05 -0700

    test

---
+++ calculator.java
@@ -0,0 +1,10 @@
+package calc;
+
+public class calculator {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        int y =9;
+        System.out.println(y);
+    }
+}
