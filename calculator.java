4191759c5e491102f22b86ff51181e7481db959d
brianjohn97 <johnson.brian997@gmail.com>
2022-09-28 13:31:50 -0700

    fixed the test

--- calculator.java
+++ calculator.java
@@ -3,8 +3,5 @@ package calc;
 public class calculator {
     public static void main(String[] args) {
         System.out.println("hello world");
-
-        int y =9;
-        System.out.println(y);
     }
 }
