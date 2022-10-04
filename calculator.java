b7feddd5ad4795fbfdd767f380caf786fd3de9e6
brianjohn97 <johnson.brian997@gmail.com>
2022-10-04 13:24:02 -0700

    added parser algo

--- calculator.java
+++ calculator.java
@@ -1,7 +1,65 @@
 package calc;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class calculator {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Mathemcatical expression here: ");
+        String num = sc.nextLine();
+
+        
+        
+    }
+    //got evalRPN from Rosetta Code website
+    //https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Java_2
+    private static void evalRPN(String expr){
+        LinkedList<Double> stack = new LinkedList<Double>();
+        System.out.println("Input\tOperation\tStack after");
+        for (String token : expr.split("\\s")){
+            System.out.print(token + "\t");
+            if (token.equals("*")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand * secondOperand);
+            } else if (token.equals("/")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand / secondOperand);
+            } else if (token.equals("-")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand - secondOperand);
+            } else if (token.equals("+")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(firstOperand + secondOperand);
+            } else if (token.equals("^")) {
+                System.out.print("Operate\t\t");
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+                stack.push(Math.pow(firstOperand, secondOperand));
+            } else {
+                System.out.print("Push\t\t");
+                try {
+                    stack.push(Double.parseDouble(token+""));
+                } catch (NumberFormatException e) {
+                        System.out.println("\nError: invalid token " + token);
+                        return;
+                }
+            }
+            System.out.println(stack);
+        }
+        if (stack.size() > 1) {
+            System.out.println("Error, too many operands: " + stack);
+            return;
+        }
+        System.out.println("Final answer: " + stack.pop());
     }
 }
