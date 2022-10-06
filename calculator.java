3c7399f9d9968b9af18cb0fc86cc591be73bc766
brianjohn97 <johnson.brian997@gmail.com>
2022-10-06 16:18:47 -0700

    added the infixToPostFix algo

--- calculator.java
+++ calculator.java
@@ -1,6 +1,7 @@
 package calc;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 import java.lang.Math;
 
 public class calculator {
@@ -8,10 +9,62 @@ public class calculator {
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Mathemcatical expression here: ");
-        String num = "5 c 5 s +"; 
+        String num = "5.5 10 + "; 
                
         evalRPN(num);
     }
+
+    //got infixToPostFix from rosettacode.org website
+    //https://rosettacode.org/wiki/Parsing/Shunting-yard_algorithm#Java
+    static String infixToPostfix(String infix) {
+        /* To find out the precedence, we take the index of the
+           token in the ops string and divide by 2 (rounding down). 
+           This will give us: 0, 0, 1, 1, 2 */
+        final String ops = "-+/*^";
+
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> s = new Stack<>();
+
+        for (String token : infix.split("\\s")) {
+            if (token.isEmpty())
+                continue;
+            char c = token.charAt(0);
+            int idx = ops.indexOf(c);
+
+            // check for operator
+            if (idx != -1) {
+                if (s.isEmpty())
+                    s.push(idx);
+          
+                else {
+                    while (!s.isEmpty()) {
+                        int prec2 = s.peek() / 2;
+                        int prec1 = idx / 2;
+                        if (prec2 > prec1 || (prec2 == prec1 && c != '^'))
+                            sb.append(ops.charAt(s.pop())).append(' ');
+                        else break;
+                    }
+                    s.push(idx);
+                }
+            } 
+            else if (c == '(') {
+                s.push(-2); // -2 stands for '('
+            } 
+            else if (c == ')') {
+                // until '(' on stack, pop operators.
+                while (s.peek() != -2)
+                    sb.append(ops.charAt(s.pop())).append(' ');
+                s.pop();
+            }
+            else {
+                sb.append(token).append(' ');
+            }
+        }
+        while (!s.isEmpty())
+            sb.append(ops.charAt(s.pop())).append(' ');
+        return sb.toString();
+    }
+
     //got evalRPN from Rosetta Code website
     //https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Java_2
     private static void evalRPN(String expr){
@@ -57,7 +110,10 @@ public class calculator {
                 double operand = stack.pop();
                 operand = Math.toRadians(operand);
                 stack.push(Math.tan(operand));
-            //}
+            }else if (token.equals("m")){
+                double operand = stack.pop();
+                operand = operand * -1;
+                stack.push(operand);
             } else {
                 System.out.print("Push\t\t");
                 try {
